817d895355846917e1f52bee7b1859fa93d1534c
Elliot Huntington <elliot.huntington@gmail.com>
2022-12-22 11:03:37 -0700

    If you want the subscription to close you need to dispose of it
    Issue resolved

--- src/main/java/com/github/axiopisty/mre/reactive/kafka/consumer/shutdown/demo/KafkaMessageEventPropagator.java
+++ src/main/java/com/github/axiopisty/mre/reactive/kafka/consumer/shutdown/demo/KafkaMessageEventPropagator.java
@@ -2,9 +2,12 @@ package com.github.axiopisty.mre.reactive.kafka.consumer.shutdown.demo;
 
 import com.github.axiopisty.mre.reactive.kafka.consumer.shutdown.demo.kafka.consumer.KafkaMessageConsumer;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import reactor.core.Disposable;
 
 @Slf4j
 @Service
@@ -14,22 +17,32 @@ public class KafkaMessageEventPropagator {
   
   private final ApplicationEventPublisher publisher;
   
+  private Optional<Disposable> disposable;
+  
   public KafkaMessageEventPropagator(KafkaMessageConsumer consumer, ApplicationEventPublisher publisher) {
     this.consumer = consumer;
     this.publisher = publisher;
   }
   
+  @PreDestroy
+  public void cleanup() {
+    log.debug("KafkaMessageEventPropagator.cleanup - disposing of subscription");
+    disposable.ifPresent(x -> x.dispose());
+  }
+  
   @PostConstruct
   public void postConstruct() {
-    log.debug("KafkaMessageEventPropagator - created");
-    consumer
+    log.debug("KafkaMessageEventPropagator.postConstruct - created");
+    disposable = Optional.of(
+      consumer
       .consume()
       .doOnNext(x -> {
         log.info("Consumed Kafka Message. Propagating it into the Spring ApplicationEventPublisher: {}", x);
         publisher.publishEvent(x);
       })
       .doOnError(t -> log.warn("Unable to consume from Kafka because : {}", t.getMessage()))
-      .subscribe();
+      .subscribe()
+    );
   }
   
 }
